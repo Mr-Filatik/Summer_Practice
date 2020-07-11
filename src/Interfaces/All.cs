e8834e8ba3bd24cf6436f145eabd39d2e059c0b5
PC-FILATIK\filat <filatov23wlad@gmail.com>
2020-07-11 17:22:39 +0400

    Create Interface

---
+++ src/Interfaces/All.cs
@@ -0,0 +1,26 @@
+﻿using AuktionEPAM.ENTITY;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces
+{
+    class All
+    {
+
+    }
+
+    public interface ILotsManager
+    {
+        void AddLot(Lot lot);
+        ICollection<Lot> SelectAllLots();
+        void DeleteLot(int id_lot);
+    }
+
+    public interface ILotsStorage
+    {
+        void AddLot(Lot shop);
+        ICollection<Lot> SelectAllLots();
+        void DeleteLot(int id_lot);
+    }
+}
