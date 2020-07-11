17429f382221c269ea0744c5cca200cf723549a8
PC-FILATIK\filat <filatov23wlad@gmail.com>
2020-07-11 17:21:24 +0400

    Create BLL

---
+++ src/AuktionEPAM.BLL/LotsManager.cs
@@ -0,0 +1,32 @@
+﻿using AuktionEPAM.DAL;
+using AuktionEPAM.ENTITY;
+using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuktionEPAM.BLL
+{
+    public class ILotsManager : Interfaces.ILotsManager
+    {
+        public static ILotsStorage iLotStorage;
+        
+        public void AddLot(Lot lot)
+        {
+            iLotStorage.AddLot(lot);
+        }
+        public void DeleteLot(int number)
+        {
+            iLotStorage.DeleteLot(number);
+        }
+        public ICollection<Lot> SelectAllLots()
+        {
+            return iLotStorage.SelectAllLots();
+        }
+
+        public ILotsManager()
+        {
+            iLotStorage = new LotsStorage();
+        }
+    }
+}
