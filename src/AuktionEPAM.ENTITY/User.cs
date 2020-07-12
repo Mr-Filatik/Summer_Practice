2992e2fcdd83f921546dcd2d883bab30a66665e3
PC-FILATIK\filat <filatov23wlad@gmail.com>
2020-07-12 23:06:54 +0400

    Create user entity

---
+++ src/AuktionEPAM.ENTITY/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuktionEPAM.ENTITY
+{
+    public class User
+    {
+        public int Id_user { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+        public User() { }
+    }
+}
