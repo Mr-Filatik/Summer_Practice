57e7a91ad6b3ed76a2244a6afd4ece254de1d597
PC-FILATIK\filat <filatov23wlad@gmail.com>
2020-07-12 23:08:06 +0400

    Upgrade user interface

--- src/AuktionEPAM.PL/Program.cs
+++ src/AuktionEPAM.PL/Program.cs
@@ -20,33 +20,50 @@ namespace AuktionEPAM.PL
         private static void SelectByUser()
         {
             Console.WriteLine("Выберите опцию");
-            Console.WriteLine("1 - создать лот");
-            Console.WriteLine("2 - показать все лоты");
-            Console.WriteLine("3 - повысить цену");
-            Console.WriteLine("0 - выйти");
+            Console.WriteLine("Введите 1 чтобы создать лот");
+            Console.WriteLine("Введите 2 чтобы показать все доступные лоты");
+            Console.WriteLine("Введите 3 чтобы показать созданные мною лоты");
+            Console.WriteLine("Введите 4 чтобы предложить цену");
+            Console.WriteLine("Введите 5 чтобы удалить лот");
+            Console.WriteLine("Введите 0 чтобы выйти из приложения");
             var input = Console.ReadLine();
             if (uint.TryParse(input, out uint result)
                 && result >= 0
-                && result <= 3)
+                && result <= 5)
             {
+                int id_lot;
+                ICollection<Lot> lots = null;
                 switch (result)
                 {
                     case 0:
                         return;
                     case 1:
+                        Console.WriteLine("Введите название добавляемого лота");
                         string name = Console.ReadLine();
+                        Console.WriteLine("Введите цену для добавляемого лота");
                         int price = Convert.ToInt32(Console.ReadLine());
-                        var newLot = new Lot(1, 1, name, price, DateTime.Now, false);
+                        var newLot = new Lot(0, 1, name, price, DateTime.Now, false);
                         ILotsManager.AddLot(newLot);
                         SelectByUser();
                         break;
                     case 2:
-                        ICollection<Lot> lots = ILotsManager.SelectAllLots();
+                        lots = ILotsManager.SelectAllLots();
                         ShowLots(lots);
                         SelectByUser();
                         break;
                     case 3:
-                        int id_lot = Convert.ToInt32(Console.ReadLine());
+                        lots = ILotsManager.SelectMyLots();
+                        ShowLots(lots);
+                        SelectByUser();
+                        break;
+                    case 4:
+                        id_lot = Convert.ToInt32(Console.ReadLine());
+                        ILotsManager.DeleteLot(id_lot);
+                        SelectByUser();
+                        break;
+                    case 5:
+                        Console.WriteLine("Введите номер удяляемого лота");
+                        id_lot = Convert.ToInt32(Console.ReadLine());
                         ILotsManager.DeleteLot(id_lot);
                         SelectByUser();
                         break;
@@ -59,9 +76,9 @@ namespace AuktionEPAM.PL
             {
                 Console.WriteLine("№ {0} Наименование: {1}", i.Id_lot, i.Name);
                 Console.WriteLine("Начало - Цена: {0} Время: {1}", i.Start_price, i.Start_price);
-                Console.WriteLine("Создатель: {0} {1}", i.Name_1, i.Surname_1);
-                Console.WriteLine("Последнее - Цена: {0} Время: {1}", i.Finish_price, i.Finish_price);
-                Console.WriteLine("Желающий: {0} {1}", i.Name_2, i.Surname_2);
+                Console.WriteLine("Создатель: {0} {1}", i.Creator.Name, i.Creator.Surname);
+                //Console.WriteLine("Последнее - Цена: {0} Время: {1}", i.Finish_price, i.Finish_price);
+                //Console.WriteLine("Желающий: {0} {1}", i.Name_2, i.Surname_2);
                 Console.WriteLine();
             }
         }
